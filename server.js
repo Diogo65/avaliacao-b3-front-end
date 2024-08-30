f5216d71872899456a7e2eda31c8f69dd0571694
Diogo SquadPremium <Diogo.SquadPremium@genial.com.vc>
2024-08-30 05:21:51 -0300

    dockerfile

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-app.use(express.static(__dirname + '/dist/avaliacao-b3'));
-app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+
-'/dist/avaliacao-b3/index.html'));});
-app.listen(process.env.PORT || 8080);
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}) );
+
+app.use( express.static(__dirname + '/client' ) );
+
+var listener = server.listen(process.env.PORT || 5000, function(){
+    console.log('Listening on port ' + listener.address().port); //Listening on port 5000
+});
